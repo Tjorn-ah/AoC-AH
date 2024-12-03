0227bf7a334ebc4751816591d13e0954c54229f4
Tjorn <74906283+Nrojt@users.noreply.github.com>
2024-12-03 10:17:46 +0100

    Day 2 easy clap (jk)

--- src/main/kotlin/day02/Day02.kt
+++ src/main/kotlin/day02/Day02.kt
@@ -4,24 +4,61 @@ import readInput
 import println
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        // Solution for part 1
-        return input.size
+    fun List<Int>.isSafeAscending(): Boolean {
+        return this.zipWithNext().all { (it.first > it.second) && (kotlin.math.abs(it.first - it.second) in 1..3) }
     }
 
-    fun part2(input: List<String>): Int {
+    fun List<Int>.isSafeDescending(): Boolean {
+        return this.zipWithNext().all { (it.first < it.second) && (kotlin.math.abs(it.first - it.second) in 1..3) }
+    }
+
+    fun part1(input: List<List<Int>>): Pair<Int, List<List<Int>>> {
+        var safeCount = 0
+        val toRemove : MutableList<List<Int>> = mutableListOf()
+        input.onEach {
+            if (it.isSafeAscending() || it.isSafeDescending()) {
+                safeCount++
+                toRemove.add(it)
+            }
+        }
+
+        // Solution for part 1
+        return Pair(safeCount,  input.filterNot { it in toRemove })
+    }
+
+    fun part2(input: List<List<Int>>): Int {
+        var (safeCount, inputFixed) = part1(input)
+
+        inputFixed.forEach {
+            for(i in it.indices){
+                val listWithOneRemoved: List<Int> = it.toMutableList().apply { removeAt(i) }.toList()
+                println(listWithOneRemoved)
+                    if (listWithOneRemoved.isSafeAscending() || listWithOneRemoved.isSafeDescending()) {
+                        safeCount++
+                        break
+                    }
+            }
+        }
+
         // Solution for part 2
-        return input.size
+        return safeCount
+    }
+
+    fun parseInput(input: List<String>): List<List<Int>> {
+        val array = input.map { it.split(" ").map { it2 -> it2.toInt() } }
+        return array
     }
 
     // Test if implementation meets criteria from the challenge description, like:
     val testInput = readInput("day02", "day02_test")
-    val testAnswerPartOne = 0
-    val testAnswerPartTwo = 0
-    check(part1(testInput) == testAnswerPartOne) { "answer to test one is wrong" }
-    check(part2(testInput) == testAnswerPartTwo) { "answer to test two is wrong" }
+    val parsedTestInput = parseInput(testInput)
+    val testAnswerPartOne = 2
+    val testAnswerPartTwo = 4
+    check(part1(parsedTestInput).first == testAnswerPartOne) { "answer to test one is wrong" }
+    check(part2(parsedTestInput) == testAnswerPartTwo) { "answer to test two is wrong" }
 
     val input = readInput("day02", "day02")
-    part1(input).println()
-    part2(input).println()
+    val parsedInput = parseInput(input)
+    part1(parsedInput).println()
+    part2(parsedInput).println()
 }

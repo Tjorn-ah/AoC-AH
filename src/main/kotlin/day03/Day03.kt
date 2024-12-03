f7cb52b8c1d76be5d0825d0e900ab3a874f7a542
Tjorn <74906283+Nrojt@users.noreply.github.com>
2024-12-03 12:02:42 +0100

    Day 3 part 1

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -4,24 +4,34 @@ import readInput
 import println
 
 fun main() {
+
     fun part1(input: List<String>): Int {
+        val regex = Regex(pattern = "[0-9]+")
+        val intlist = input.map { regex.findAll(it).map { it2 -> it2.value.toInt() }.toList()}
         // Solution for part 1
-        return input.size
+        return intlist.sumOf { it.reduce { acc, i -> acc * i  } }
     }
 
     fun part2(input: List<String>): Int {
         // Solution for part 2
-        return input.size
+        return 0
+    }
+
+    fun useRegex(input: String): List<String> {
+        val regex = Regex(pattern = "mul\\([0-9]+,[0-9]+\\)")
+        return  regex.findAll(input).map { it.value }.toList()
     }
 
     // Test if implementation meets criteria from the challenge description, like:
-    val testInput = readInput("day03", "day03_test")
-    val testAnswerPartOne = 0
+    val testInput = readInput("day03", "day03_test").joinToString()
+    val mulTestInput = useRegex(testInput)
+    val testAnswerPartOne = 161
     val testAnswerPartTwo = 0
-    check(part1(testInput) == testAnswerPartOne) { "answer to test one is wrong" }
-    check(part2(testInput) == testAnswerPartTwo) { "answer to test two is wrong" }
+    check(part1(mulTestInput) == testAnswerPartOne) { "answer to test one is wrong" }
+    check(part2(mulTestInput) == testAnswerPartTwo) { "answer to test two is wrong" }
 
-    val input = readInput("day03", "day03")
-    part1(input).println()
-    part2(input).println()
+    val input = readInput("day03", "day03").joinToString()
+    val mulInput = useRegex(input)
+    part1(mulInput).println()
+    part2(mulInput).println()
 }
